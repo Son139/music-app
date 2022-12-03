ab00c198e78b8fec7e00d712c89c408a4827d6dd
son tran <sontran1139@gmail.com>
2022-12-03 10:21:34 +0700

    update

--- assests/main.js
+++ assests/main.js
@@ -270,49 +270,4 @@ displayTimer();
 rangeBar.value = 0;
 setInterval(displayTimer , 200); 
 
-//==============================================
-//                  DARK THEME
-//==============================================
-const ball = document.querySelector('.ball');
-const blackThemeBtn = document.querySelector('.check');
-const blackThemeCanvas = document.getElementById('container');
-blackThemeBtn.addEventListener('click', function() {
-    if (blackThemeCanvas.classList.contains('dark-theme')) {
-        blackThemeCanvas.classList.remove('dark-theme');
-        ball.style.left = '2px';
-    }
-    else  {
-        blackThemeCanvas.classList.add('dark-theme');
-        ball.style.left = '22px'
-    }
-});
 
-//==============================================
-//                  MOBILE JS
-//==============================================
-
-const barLeft = document.querySelector('.bars-left');
-const barRight = document.querySelector('.bars-right');
-const sideBar = document.querySelector('#sidebar');
-const navBar = document.querySelector('#navbar');
-const hideNavBar = document.querySelector('.js-hide-navbar');
-const hideSideBar = document.querySelector('.js-hide-sidebar');
-
-barLeft.addEventListener('click', function() {
-    navBar.classList.add('active');
-    if (sideBar.classList.contains('active')) {
-        sideBar.classList.remove('active');
-    }
-})
-hideNavBar.addEventListener('click', function() {
-    navBar.classList.remove('active')
-})
-barRight.addEventListener('click', function() {
-    sideBar.classList.add('active');
-    if (navBar.classList.contains('active')) {
-        navBar.classList.remove('active');
-    }
-})
-hideSideBar.addEventListener('click', function() {
-    sideBar.classList.remove('active');
-})
